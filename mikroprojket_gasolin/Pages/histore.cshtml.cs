5e70cd6249a1667e9d2c69fad833f7811dd97af9
Martin Egholm Sørensen <mas025@edu.zealand.dk>
2025-03-18 13:41:44 +0100

    band histore og memorabilia tilføjet

---
+++ mikroprojket_gasolin/Pages/histore.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mikroprojket_gasolin.Pages
+{
+    public class historeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
