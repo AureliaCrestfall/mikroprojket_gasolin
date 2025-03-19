f3a23eeea22a164d7f21adbc3440109ffff3a3ff
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-03-19 10:45:31 +0100

    Søren

---
+++ mikroprojket_gasolin/Pages/SørenBerlev.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mikroprojket_gasolin.Pages
+{
+    public class SørenBerlevModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
