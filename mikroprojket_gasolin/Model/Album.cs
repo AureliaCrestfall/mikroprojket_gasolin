189f7e258787bf9227b74e8d8001fa2e737b0969
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-03-20 09:38:14 +0100

    c# på kim larsen siden

---
+++ mikroprojket_gasolin/Model/Album.cs
@@ -0,0 +1,18 @@
+﻿namespace mikroprojket_gasolin.Model
+{
+    public class Album
+    {
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string ImagePath { get; set; }
+        
+
+        
+        public Album(string name, string year, string imagePath)
+        {
+            Name = name;
+            Year = year;
+            ImagePath = imagePath;
+        }
+    }
+}
--- mikroprojket_gasolin/Pages/KimLarsen.cshtml.cs
+++ mikroprojket_gasolin/Pages/KimLarsen.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using mikroprojket_gasolin.Model;
 
 namespace mikroprojket_gasolin.Pages
 {
     public class KimLarsenModel : PageModel
     {
+
+        private Album[] albums = new Album[4];
+
+
+        private readonly ILogger<KimLarsenModel> _logger;
+
+        public Album[] Albums { get => albums; set => albums = value; }
+
+        public KimLarsenModel(ILogger<KimLarsenModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            albums[0] = new Album("Gasolin'", "1971", "Gasolin1.png");
+
+            albums[1] = new Album("Gasolin' 2", "1973", "Gasolin'_2.jpg");
+
+            albums[2] = new Album("Gasolin' 3", "1975", "Gasolin'_3.jpg");
+            albums[3] = new Album("Gasolin' 4", "1978", "Gasolin'_4.jpg");
+
         }
     }
 }
